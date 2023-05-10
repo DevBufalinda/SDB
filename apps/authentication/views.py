daf9e9a222513bc004c708a5cda8bb4fdaeac840
Sinai <sguerra@bufalinda.com>
2023-05-10 15:17:08 -0400

    Modificación de Login

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -24,9 +24,9 @@ def login_view(request):
                 login(request, user)
                 return redirect("/")
             else:
-                msg = 'Invalid credentials'
+                msg = 'Usuario y/o contraseña incorrecta'
         else:
-            msg = 'Error validating the form'
+            msg = 'Introduzca usuario y contraseña'
 
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
