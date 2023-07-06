e1eca294cd8340450d8bfba312d51f490c5063a7
marquezluisg <lmarquez@bufalinda.com>
2023-07-06 15:23:29 -0400

    añadiendo validaciones de seriales con sweetalert

--- apps/static/assets/js/crypto-js.js
+++ apps/static/assets/js/crypto-js.js
@@ -1,6 +1,18 @@
-
-
 $('#alert_demo_8').click(function (e) {
+  e.preventDefault();
+  showDeleteConfirmationModal();
+});
+
+// Evento para mostrar la alerta de eliminación al hacer clic fuera del modal
+$(document).click(function (event) {
+  var target = $(event.target);
+  if (target.is(detailModal) && detailModal.is(':visible')) {
+    showDeleteConfirmationModal();
+  }
+});
+
+// Función para mostrar la alerta de eliminación
+function showDeleteConfirmationModal() {
   swal({
     title: '¿Deseas Eliminar Los Seriales Seleccionados?',
     text: "No podrás revertir esto.!",
@@ -15,39 +27,46 @@ $('#alert_demo_8').click(function (e) {
         text: 'Sí, Eliminar!',
         className: 'btn btn-success'
       }
-    },
-    content: {
-      element: "input",
-      attributes: {
-        placeholder: "Ingresa la contraseña",
-        type: "password",
-      },
-    },
-  }).then((password) => {
-    // Encriptamos la contraseña ingresada
-    const key = 'QCN4021420002';
-    const expectedPassword = 'U2FsdGVkX1+OeP6WJylL4G6f1yW4wzTbLXq3TIYi9D8=';
-    const encryptedPassword = CryptoJS.AES.encrypt(password, key).toString();
-
-    // Validamos la contraseña
-    if (encryptedPassword === expectedPassword) {
-      swal("¡Has Eliminado Los Seriales Con Éxito!", {
-        icon: "success",
-        buttons: {
-          confirm: {
-            className: 'btn btn-success'
+    }
+  }).then((willDelete) => {
+    if (willDelete) {
+      const tablaDatos = serialTable.DataTable().data();
+      if (tablaDatos.any()) {
+        LotSerList = [];
+        //serialesIngresados = [];
+        serialTable.DataTable().clear().draw();
+        detailModal.modal('hide');
+        console.log("Seriales Eliminados: ");
+        console.log(LotSerList);
+        swal("¡Has Eliminado Los Seriales Con Éxito!", {
+          icon: "success",
+          buttons: {
+            confirm: {
+              className: 'btn btn-success'
+            }
           }
-        }
-      });
+        });
+      } else {
+        swal("¡No Se Ha Cargado Ningun Serial Para Eliminar!", {
+          icon: "warning",
+          buttons: {
+            confirm: {
+              className: 'btn btn-success'
+            }
+          }
+        });
+      }
     } else {
-      swal("¡Contraseña incorrecta!", {
-        icon: "error",
+      console.log("Seriales Salvados: ");
+      console.log(LotSerList);
+      swal("¡Tus Seriales Han Sido Salvados!", {
         buttons: {
           confirm: {
             className: 'btn btn-success'
           }
         }
       });
+      detailModal.modal('show');
     }
   });
-});
+}
